05935a4a65300774a4347b8f1069a3b00691b430
Sarwar Habib <engr.sarwarhabib@gmail.com>
2025-08-08 23:57:48 +0600

    create user and signin user in authprovider compeleted

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { AuthContext } from './AuthContext'
 import { auth } from '../../firebase/firebase.init'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 const AuthProvider = ({children}) => {
 
-    
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
     const authInfo = {
+        createUser,
+        loginUser,
+        
         
     }
 
